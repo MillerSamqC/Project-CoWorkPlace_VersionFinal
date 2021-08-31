34d2899ef23a2901c70017bbce229b82e0ce6a6f
jdarguello11 <juanda4222@gmail.com>
2021-08-31 14:58:22 -0500

    Adición de la clase Productos

---
+++ Productos.py
@@ -0,0 +1,9 @@
+class Producto:
+	nombre = ""
+	precio = 0
+	presentacion = ""
+
+	def __init__(self, nombre, precio, presentacion):
+		self.nombre = nombre
+		self.precio = precio
+		self.presentacion = presentacion
