ab0488d50e55a55a39aa839417eac66d2ff1b5a0
MillerSamqC <57202767+MillerSamqC@users.noreply.github.com>
2021-09-08 14:39:36 -0500

    version 0.0

--- Productos.py
+++
@@ -1,9 +0,0 @@
-class Producto:
-	nombre = ""
-	precio = 0
-	presentacion = ""
-
-	def __init__(self, nombre, precio, presentacion):
-		self.nombre = nombre
-		self.precio = precio
-		self.presentacion = presentacion
---
+++ coworkplace/coworkplace/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for coworkplace project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coworkplace.settings')
+
+application = get_asgi_application()
