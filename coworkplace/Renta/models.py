d89718cc8282d439349e3d305f3850c384b5c1d6
Ferney F Roa H <ferney.roa@gmail.com>
2021-10-04 18:58:24 -0500

    Cambios

--- coworkplace/Renta/models.py
+++ coworkplace/Renta/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from Catalogo.models import Lugar
+from Usuarios.models import Perfil
 from datetime import date, datetime
 
 # Create your models here.
 
 #Clase para la Reserva
 class Reserva(models.Model):
-    idLugar = models.ForeignKey(Lugar, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Perfil, on_delete=models.SET_NULL, null=True, blank=True)
+    idLugar = models.ForeignKey(Lugar, on_delete=models.CASCADE,  null=True, blank=True)
     Fecha_ini_reserva = models.DateField(auto_now_add=True)
     Fecha_fin_reserva = models.DateField(auto_now_add=True)
     Pago_confirmado = models.BooleanField(blank = True, null=True)
     Estado_reserva = models.BooleanField(blank = True, null=True)
 
     def __str__(self):
-        return self.Estado_reserva
+        return self.usuario.__str__() + ' - ' + self.idLugar.__str__() + ' --> ' + str(self.Fecha_ini_reserva)
 
     def CancelarReserva(self):
         self.Estado_reserva = False
@@ -30,6 +32,9 @@ class Reserva(models.Model):
 
 #Clase para los pagos
 class Pago(models.Model):
-    idReserva = models.ForeignKey(Reserva, on_delete=models.CASCADE)
+    idReserva = models.ForeignKey(Reserva, on_delete=models.CASCADE,  null=True, blank=True)
     Valor_pago = models.FloatField(blank = True, null=True)
     Fecha_pago = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Pago de ' + self.idReserva.usuario.__str__() + ' por ' + self.idReserva.idLugar.__str__() 
