71982361e706ec2c73e61167a16bae3f2563868a
MillerSamqC <57202767+MillerSamqC@users.noreply.github.com>
2021-10-07 19:25:46 -0500

    Cambios en la pagina web

---
+++ WebPage/header.php
@@ -0,0 +1,24 @@
+<header>
+        <nav> <!-- Barra de menú-->
+            <img class="logo" src="img/LogoXx.png" alt="logo" width="30%" align="left">            
+            <a href="index.html">Inicio</a>
+            <a href="reserva.html">Reserva</a>
+            <a href="acerca_de.html">Acerca de</a>
+            <a href="contactenos.html">Contactenos</a>
+            <a href="sesion.html" data-bs-toggle="modal" data-bs-target="#modalSignin">Inicia Sesion</a>
+            <a href="#">Registrate</a>
+        </nav>
+        <section class="textos-header">
+            <h1>CoWorkPlace</h1>
+            <h3>Otra forma de trabajar</h3>
+            <br><br><br><br><br>
+        </section>       
+
+        <!-- Curva de la ola (Wave) -> https://smooth.ie/blogs/news/svg-wavey-transitions-between-sections-->
+        <div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+                style="height: 100%; width: 100%;">
+                <path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+                    style="stroke: none; fill: #fff;"></path>
+            </svg>
+        </div>               
+    </header>
